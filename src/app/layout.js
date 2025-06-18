b4bd92d8d545cd470fda8d92d44b6ff13dfde463
zikriramdani <zikriramdani@gmail.com>
2025-06-18 12:09:44 +0700

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -51,14 +51,6 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Poppins:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <UseHeads
         title={metadata.title}
         description={metadata.description}
