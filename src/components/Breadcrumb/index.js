9160e743932b2244942aab0c0582a15566058ca4
zikriramdani <zikriramdani@gmail.com>
2025-06-19 12:48:17 +0700

    Fix: Responsive

--- src/components/Breadcrumb/index.js
+++ src/components/Breadcrumb/index.js
@@ -23,33 +23,34 @@ const Breadcrumb = ({ className = '', containerClassName = '', isLoading = false
     <section className={`${className && className + ' pt-3'}`}>
       <div className={`container ${containerClassName}`}>
         <nav aria-label="breadcrumb">
-          <ol className="breadcrumb mb-0 text-truncate" style={{ flexWrap: 'nowrap' }}>
-            <li className="breadcrumb-item">
-              {isLoading ? (
-                <Skeleton width={60} height={16} />
-              ) : (
-                <Link href="/" className="">
-                  Beranda
-                </Link>
-              )}
+          <ol
+            className="breadcrumb mb-0 d-flex flex-nowrap overflow-auto"
+            style={{
+              whiteSpace: 'nowrap'
+            }}>
+            {/* Item pertama */}
+            <li className="breadcrumb-item flex-shrink-0">
+              {isLoading ? <Skeleton width={60} height={16} /> : <Link href="/">Beranda</Link>}
             </li>
 
+            {/* Skeleton loading (opsional) */}
             {isLoading ? (
-              <Fragment>
-                <li className="breadcrumb-item">
+              <>
+                <li className="breadcrumb-item flex-shrink-0">
                   <Skeleton width={100} height={16} />
                 </li>
-                <li className="breadcrumb-item w-75">
-                  <Skeleton width={`100%`} height={16} />
+                <li className="breadcrumb-item flex-shrink-0 w-75">
+                  <Skeleton width="100%" height={16} />
                 </li>
-              </Fragment>
+              </>
             ) : (
               segments.map((item, idx) => (
                 <li
                   key={idx}
-                  className={`breadcrumb-item ${
-                    idx === segments.length - 1 ? 'active text-truncate w-50' : ''
-                  }`}
+                  className={`breadcrumb-item text-nowrap text-truncate ${idx === segments.length - 1 ? 'active' : ''}`}
+                  style={{
+                    flexShrink: 0
+                  }}
                   aria-current={idx === segments.length - 1 ? 'page' : undefined}>
                   {idx !== segments.length - 1 ? (
                     <Link href={item.href} className="text-primary">
